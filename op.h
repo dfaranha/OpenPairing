2303f55eac9e02dcdb173b3d17c16de69733bd98
Diego F. Aranha <dfaranha@ic.unicamp.br>
2016-02-02 04:52:56 -0200

    Refactor to use OpenSSL style.

--- op.h
+++ op.h
@@ -1,6 +1,6 @@
 /*
- * OpenPairing is an implementation of a bilinear pairing over OpenSSL
- * Copyright (C) 2015 OpenPairing Authors
+ * OpenPairing is an implementation of a cryptographic pairing over OpenSSL
+ * Copyright (C) 2015 MIRACL
  *
  * This file is part of OpenPairing. OpenPairing is legal property of its
  * developers, whose names are not listed here. Please refer to the COPYRIGHT
@@ -20,16 +20,16 @@
  * along with OpenPairing. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <openssl/bn.h>
-#include <openssl/ec.h>
+#ifndef HEADER_OP_H
+# define HEADER_OP_H
 
-#include "op_test.h"
-#include "op_bench.h"
+# include "ec_lcl.h"
+# include "openssl/ec.h"
+# include "openssl/bn.h"
 
-#ifndef OP_H
-#define OP_H
+# ifdef  __cplusplus
+extern "C" {
+# endif
 
 typedef struct _FP2 {
 	BIGNUM f[2];
@@ -43,18 +43,19 @@ typedef struct _FP12 {
 	FP6 f[2];
 } FP12;
 
-typedef struct _OP_CTX {
-	BIGNUM *prime;
-	BIGNUM *one;
-	BN_MONT_CTX *mn;
-	BN_CTX *bn;
+/** Stores information regarding the groups involved in pairing computation. */
+struct pairing_group_st {
 	EC_GROUP *ec;
-	EC_POINT *g1;
+	BIGNUM *field;
+	BN_CTX *bn;
 	FP2 *g2x;
 	FP2 *g2y;
-} OP_CTX;
+};
 
-extern OP_CTX ctx;
+/** Convenient type to manipulate pairing groups. */
+typedef struct pairing_group_st PAIRING_GROUP;
+
+extern PAIRING_GROUP group;
 
 int op_init(void);
 void op_free(void);
@@ -63,32 +64,32 @@ unsigned long long ARCH_cycles(void);
 
 void FP2_init(FP2 *a);
 void FP2_free(FP2 *a);
-int FP2_rand(FP2 *a);
-void FP2_print(FP2 *a);
+int FP2_rand(const PAIRING_GROUP *group, FP2 *a);
+void FP2_print(const FP2 *a);
 int FP2_zero(FP2 *a);
-int FP2_cmp(FP2 *a, FP2 *b);
-void FP2_copy(FP2 *a, FP2 *b);
-int FP2_is_zero(FP2 *a);
-int FP2_add(FP2 *r, FP2 *a, FP2 *b);
-int FP2_sub(FP2 *r, FP2 *a, FP2 *b);
-int FP2_neg(FP2 *r, FP2 *a);
-int FP2_mul_unr(FP2 *r, FP2 *a, FP2 *b);
-int FP2_rdc(FP2 *r, FP2 *a);
-int FP2_mul(FP2 *r, FP2 *a, FP2 *b);
-int FP2_mul_frb(FP2 *r, FP2 *a, int i);
-int FP2_mul_art(FP2 *r, FP2 *a);
-int FP2_mul2(FP2 *r, FP2 *a, FP2 *b);
-int FP2_mul_nor(FP2 *r, FP2 *a);
-int FP2_sqr(FP2 *r, FP2 *a);
-int FP2_inv(FP2 *r, FP2 *a);
-int FP2_inv_uni(FP2 *r, FP2 *a);
-int FP2_conv_uni(FP2 *r, FP2 *a);
-int FP2_inv_sim(FP2 *r, FP2 *s, FP2 *a, FP2 *b);
+int FP2_cmp(const FP2 *a, const FP2 *b);
+void FP2_copy(FP2 *a, const FP2 *b);
+int FP2_is_zero(const FP2 *a);
+int FP2_add(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, const FP2 *b);
+int FP2_sub(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, const FP2 *b);
+int FP2_neg(const PAIRING_GROUP *group, FP2 *r, const FP2 *a);
+int FP2_mul(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, const FP2 *b, BN_CTX *ctx);
+int FP2_mul_frb(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, int i, BN_CTX *ctx);
+int FP2_mul_art(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, BN_CTX *ctx);
+int FP2_mul_nor(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, BN_CTX *ctx);
+int FP2_sqr(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, BN_CTX *ctx);
+int FP2_inv(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, BN_CTX *ctx);
+int FP2_inv_uni(const PAIRING_GROUP *group, FP2 *r, const FP2 *a);
+int FP2_conv_uni(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, BN_CTX *ctx);
+int FP2_inv_sim(const PAIRING_GROUP *group, FP2 *r, FP2 *s, const FP2 *a, const FP2 *b, BN_CTX *ctx);
+int FP2_mul_unr(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, const FP2 *b, BN_CTX *ctx);
+int FP2_rdc(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, BN_CTX *ctx);
+int FP2_mul2(const PAIRING_GROUP *group, FP2 *r, const FP2 *a, const FP2 *b, BN_CTX *ctx);
 
 void FP6_init(FP6 *a);
 void FP6_free(FP6 *a);
 
-int FP6_rand(FP6 *a);
+int FP6_rand(PAIRING_GROUP *group, FP6 *a);
 void FP6_print(FP6 *a);
 int FP6_zero(FP6 *a);
 int FP6_cmp(FP6 *a, FP6 *b);
@@ -109,7 +110,7 @@ int FP6_inv(FP6 *r, FP6 *a);
 void FP12_init(FP12 *a);
 void FP12_free(FP12 *a);
 
-int FP12_rand(FP12 *a);
+int FP12_rand(PAIRING_GROUP *group, FP12 *a);
 void FP12_print(FP12 *a);
 int FP12_zero(FP12 *a);
 int FP12_cmp(FP12 *a, FP12 *b);
@@ -125,10 +126,13 @@ int FP12_sqr_pck(FP12 *r, FP12 *a);
 int FP12_inv(FP12 *r, FP12 *a);
 int FP12_inv_uni(FP12 *r, FP12 *a);
 int FP12_cyc(FP12 *r, FP12 *a);
-int FP12_back(FP12 *r, FP12 *s, FP12 *a, FP12 *b);
+int FP12_back(FP12 *r, FP12 *s, FP12 *a, FP12 *b, BN_CTX *ctx);
 int FP12_frb(FP12 *r, FP12 *a);
 int FP12_exp_cyc(FP12 *r, FP12 *a);
 
-int op_map(FP12 *r, EC_POINT *g, FP2 *x, FP2 *y);
+int op_map(FP12 *r, const EC_POINT *g, const FP2 *x, const FP2 *y);
 
-#endif /* OP_H */
+#ifdef  __cplusplus
+}
+#endif
+#endif
